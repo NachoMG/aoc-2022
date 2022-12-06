99b716c3a68ee1f7e63a2bec3e6069fcf72ea862
Nacho <nacho.m.garcia@gmail.com>
2022-12-06 15:14:44 +0100

    Day 6

---
+++ 6/part1.js
@@ -0,0 +1,17 @@
+const fs = require('fs').promises;
+
+const getStartMarkerIndex = (datastream, markerLength) => {
+  for (let i = 0; i < datastream.length; i++) {
+    const startMarkerCandidate = datastream.substring(i, i + markerLength);
+    if (new Set(startMarkerCandidate).size === markerLength) {
+      return i + markerLength;
+    }
+  }
+};
+
+const main = async () => {
+  const datastream = await fs.readFile('input.txt', 'utf-8');
+  console.log(getStartMarkerIndex(datastream, 4));
+};
+
+main();
